c1a30a5cbedd700d48189cf1d64d9e2e7b3ada2e
oluwasao <lekibaba@gmail.com>
2018-07-21 22:34:36 +0100

    first commit

---
+++ src/AddressProcessor.Tests/AddressFileProcessorTests.cs
@@ -0,0 +1,41 @@
+﻿using AddressProcessing.Address;
+using AddressProcessing.Address.v1;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace AddressProcessing.Tests
+{
+    [TestFixture]
+    public class AddressFileProcessorTests
+    {
+        private FakeMailShotService _fakeMailShotService;
+        //make variable readlonly to dynamically set the current directory
+        private readonly string TestInputFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"test_data\contacts.csv");
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeMailShotService = new FakeMailShotService();
+        }
+
+        [Test]
+        public void Should_send_mail_using_mailshot_service()
+        {
+            var processor = new AddressFileProcessor(_fakeMailShotService);
+            processor.Process(TestInputFile);
+
+            Assert.That(_fakeMailShotService.Counter, Is.EqualTo(229));
+        }
+
+        internal class FakeMailShotService : IMailShot
+        {
+            internal int Counter { get; private set; }
+
+            public void SendMailShot(string name, string address)
+            {
+                Counter++;
+            }
+        }
+    }
+}
---
+++ src/AddressProcessor/CSV/CSVReader.cs
@@ -0,0 +1,13 @@
+﻿using AddressProcessing.CSV.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressProcessing.CSV
+{
+   public class CSVReader : ICSVReader
+    {
+    }
+}
